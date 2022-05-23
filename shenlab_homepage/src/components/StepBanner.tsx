3d4c250034b2efd077c70778d6970eb1c33e494e
SilverN <893325022@qq.com>
2022-05-23 17:22:46 +0800

    update cover picture

--- shenlab_homepage/src/components/StepBanner.tsx
+++ shenlab_homepage/src/components/StepBanner.tsx
@@ -2,9 +2,8 @@ import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
 import React from 'react';
-import { Button, Slider, Stack } from '@mui/material';
+import { Button, Slider, Stack, Typography } from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
@@ -39,20 +38,41 @@ export default function StepBanner(props: any) {
         setActiveStep(step);
     };
     return (
-        <Box sx={{ height: '30%', width: '70%', marginLeft: 'auto', marginRight: 'auto' }}>
+        <Box sx={{ height: '30%', width: '90%', marginLeft: 'auto', marginRight: 'auto' }}>
             <Box sx={styles.root}>
                 <AutoPlaySwipeableViews index={activeStep}
                     onChangeIndex={handleStepChange}
                     enableMouseEvents
-                    interval={50000}
+                    interval={10000}
                     direction='incremental'
                     ignoreNativeScroll={true}
                 >
-                    {steps.map((step: string, idx: number) => (
-                        <Box sx={{ textAlign: 'center', overflow: 'hidden', display: 'flex', justifyContent: 'center' }} key={idx}>
-                            <img src={step} style={{ maxHeight: 400 }} />
-                        </Box>
-                    ))}
+                    {steps.map((step: any, idx: number) => {
+                        if (step.type == 'photo') {
+                            return (
+                                <Box sx={{ textAlign: 'center', overflow: 'hidden', display: 'flex', justifyContent: 'center' }} key={idx}>
+                                    <img src={step.url} style={{ maxHeight: 400 }} />
+                                </Box>
+                            )
+                        }
+                        if (step.type == 'figure') {
+                            return (
+                                <Box sx={{ textAlign: 'center', overflow: 'hidden', display: 'flex', justifyContent: 'center' }} key={idx}>
+                                    <Stack direction="row" alignItems={"center"} spacing={2}>
+                                        <img src={step.url} style={{ maxHeight: 400, maxWidth: "65%" }} />
+                                        <Box sx={{ textAlign: 'left', marginTop: 'auto', marginBottom: 'auto' }} component='div'>
+                                            <Typography variant="h5" gutterBottom component="div">
+                                                {step.title}
+                                            </Typography>
+                                            <Typography variant="body1" gutterBottom component="div">
+                                                {step.text}
+                                            </Typography>
+                                        </Box>
+                                    </Stack>
+                                </Box>
+                            )
+                        }
+                    })}
                 </AutoPlaySwipeableViews>
             </Box>
 
