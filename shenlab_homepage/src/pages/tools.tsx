a1d45d8d6f4c3ba200d0083a69d3bef45b95a6a4
SilverN <893325022@qq.com>
2023-04-05 13:05:49 +0800

    update link

--- shenlab_homepage/src/pages/tools.tsx
+++ shenlab_homepage/src/pages/tools.tsx
@@ -1,17 +1,18 @@
-import { NextPage } from 'next';
-import Link from 'next/link';
-import Layout from '../components/Layout';
-import styles from '../styles/Publish.module.css'
+import { NextPage } from "next";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import styles from "../styles/Publish.module.css";
 const Tools: NextPage = () => (
-    <Layout>
-        <h2>Public Tools</h2>
-        <div className={styles.row}>
-
-        <li>
-        <b>MAGPIE</b> <Link href="/biotools/index.html/"> MAGPIE</Link>
-        </li>
-        </div>
-
-    </Layout>
-)
-export default Tools
+  <Layout>
+    <h2>Public Tools</h2>
+    <div className={styles.row}>
+      <li>
+        <b>MAGPIE</b>{" "}
+        <Link href="http://tools.shenlab-genomics.org/tools/MAGPIE">
+          MAGPIE
+        </Link>
+      </li>
+    </div>
+  </Layout>
+);
+export default Tools;
