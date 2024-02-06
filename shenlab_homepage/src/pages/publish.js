8115a656c82e2ab8f25f6c4ecfcd26a69bd01c4e
SilverN <893325022@qq.com>
2024-02-06 15:02:27 +0800

    checkpoint

--- shenlab_homepage/src/pages/publish.js
+++ shenlab_homepage/src/pages/publish.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 import styles from "../styles/Publish.module.css";
+import { Button, Container, Grid, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 const patents = [
   "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., Shen, N., Robertson, A.S. and Chang, A.N., Fulcrum Therapeutics Inc, 2020. P38 kinase inhibitors reduce DUX4 and downstream gene expression for the treatment of FSHD. U.S. Patent 10,537,560.",
   "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., Shen, N., Robertson, A.S. and Chang, A.N., Fulcrum Therapeutics Inc, 2020. P38 kinase inhibitors reduce DUX4 and downstream gene expression for the treatment of FSHD. Israel Patent Offiece IL273589.",
@@ -25,165 +27,223 @@ const _articles = [
     authors:
       "Liu, Y. #, Zhang, T. #, You, N., Wu, S. *, & <b>Shen, N.*</b> (2024).",
     title:
-      "MAGPIE: accurate pathogenic prediction for multiple variant types using machine learning approach",
+      "MAGPIE: accurate pathogenic prediction for multiple variant types using machine learning approach.",
     journal: "<b>Genome medicine</b>,16(1),3.",
     link: "https://doi.org/10.1186/s13073-023-01274-4",
     year: 2024,
   },
   {
-    authors: "AAA",
-    title: "BBB",
-    journal: "CCC",
-    link: "DDD",
-    year: 2024,
+    authors:
+      "Zhang, T., Jia, H., Song, T., Lv, L., Gulhan, D. C., Wang, H., Guo, W., Xi, R., Guo, H., & <b>Shen, N.</b> (2023).",
+    title:
+      "De novo identification of expressed cancer somatic mutations from single-cell RNA sequencing data.",
+    journal: "<b>Genome medicine</b>, 15(1), 115.",
+    link: "https://doi.org/10.1186/s13073-023-01269-1",
+    year: 2023,
+  },
+  {
+    authors:
+      "Wu, L., Wang, H., Chen, Y., Zhang, X., Zhang, T., <b>Shen, N.</b>, Tao, G., Sun, Z., Ding, Y. *, Wang, W. *, & Bu, J. * (2023).",
+    title:
+      "Beyond radiologist-level liver lesion detection on multi-phase contrast-enhanced CT images by deep learning.",
+    journal: "<b>iScience, 26(11)<b>, 108183.",
+    link: "https://doi.org/10.1016/j.isci.2023.108183",
+    year: 2023,
+  },
+  {
+    authors:
+      "Lv, L. #, Zhang, T. #, Jia, H., Zhang, Y., Ahsan, A., Zhao, X., Chen, T. *, Shen, Z. *, & <b>Shen, N.</b> * (2022).",
+    title:
+      "Temporally integrated transcriptome analysis reveals ASFV pathology and host response dynamics.",
+    journal: "<b>Frontiers in immunology</b>, 13, 995998.",
+    link: "https://doi.org/10.3389/fimmu.2022.995998",
+    year: 2022,
+  },
+  {
+    authors:
+      "Jin, Z., Huang, W., <b>Shen, N.</b>, Li, J., Wang, X., Dong, J., Park, P. J., & Xi, R. (2022).",
+    title: "Single-cell gene fusion detection by scFusion.",
+    journal: "<b>Nature communications</b>, 13(1), 1084.",
+    link: "https://doi.org/10.1038/s41467-022-28661-6",
+    year: 2022,
+  },
+  {
+    authors:
+      "Li, J., Cai, Z., Vaites, L. P., <b>Shen, N.</b>, Mitchell, D. C., Huttlin, E. L., Paulo, J. A., Harry, B. L. *, & Gygi, S. P. * (2021).",
+    title: "Proteome-wide mapping of short-lived proteins in human cells.",
+    journal: "<b>Molecular cell</b>, 81(22), 4722-4735.e5.",
+    link: "https://doi.org/10.1016/j.molcel.2021.09.015",
+    year: 2021,
+  },
+  {
+    authors:
+      "Rojas, L. A., Valentine, E., Accorsi, A., Maglio, J., <b>Shen, N.<b>, Robertson, A., Kazmirski, S., Rahl, P., Tawil, R., Cadavid, D., Thompson, L. A., Ronco, L., Chang, A. N., Cacace, A. M., & Wallace, O. (2020).",
+    title:
+      "p38&alpha; Regulates Expression of DUX4 in a Model of Facioscapulohumeral Muscular Dystrophy.",
+    journal:
+      "<b>The Journal of pharmacology and experimental therapeutics</b>, 374(3), 489-498.",
+    link: "https://doi.org/10.1124/jpet.119.264689",
+    year: 2020,
+  },
+  {
+    authors:
+      "<b>Shen, N.</b>, Zhao, J., Schipper, J. L., Zhang, Y., Bepler, T., Leehr, D., Bradley, J., Horton, J., Lapp, H., & Gordan, R. (2018).",
+    title:
+      "Divergence in DNA Specificity among Paralogous Transcription Factors Contributes to Their Differential In Vivo Binding.",
+    journal: "<b>Cell systems</b>, 6(4), 470-483.e8.",
+    link: "https://doi.org/10.1016/j.cels.2018.02.009",
+    year: 2018,
+  },
+  {
+    authors:
+      "Sparks, E. E., Drapek, C., Gaudinier, A., Li, S., Ansariola, M., <b>Shen, N.</b>, Hennacy, J. H., Zhang, J., Turco, G., Petricka, J. J., Foret, J., Hartemink, A. J., Gordân, R., Megraw, M., Brady, S. M., & Benfey, P. N. (2016).",
+    title:
+      "Establishment of Expression in the SHORTROOT-SCARECROW Transcriptional Cascade through Opposing Activities of Both Activators and Repressors.",
+    journal: "<b>Developmental cell</b>, 39(5), 585-596.",
+    link: "https://doi.org/10.1016/j.devcel.2016.09.031",
+    year: 2016,
+  },
+  {
+    authors:
+      "Zhou, T. #, <b>Shen, N. #</b>, Yang, L., Abe, N., Horton, J., Mann, R. S., Bussemaker, H. J., Gordân, R. *, & Rohs, R. * (2015).",
+    title:
+      "Quantitative modeling of transcription factor binding specificities using DNA shape. ",
+    journal:
+      "<b>Proceedings of the National Academy of Sciences of the United States of America</b>, 112(15), 4654-4659.",
+    link: "https://doi.org/10.1073/pnas.1422023112",
+    year: 2015,
+  },
+  {
+    authors:
+      "Gordân, R., <b>Shen, N.</b>, Dror, I., Zhou, T., Horton, J., Rohs, R. *, & Bulyk, M. L. * (2013).",
+    title:
+      "Genomic regions flanking E-box binding sites influence DNA binding specificity of bHLH transcription factors through DNA shape.",
+    journal: "<b>Cell reports</b>, 3(4), 1093-1104.",
+    link: "https://doi.org/10.1016/j.celrep.2013.03.014",
+    year: 2013,
+  },
+  {
+    authors:
+      "Chen, J., Ma, M., <b>Shen, N.</b>, Xi, J. J. *, & Tian, W. * (2013). ",
+    title:
+      "Integration of cancer gene co-expression network and metabolic network to uncover potential cancer drug targets.",
+    journal: "<b>Journal of proteome research</b>, 12(6), 2354-2364.",
+    link: "https://doi.org/10.1021/pr400162t",
+    year: 2013,
+  },
+  {
+    authors:
+      "Qi, X., Disatnik, M. H., <b>Shen, N.</b>, Sobel, R. A., & Mochly-Rosen, D. (2011).",
+    title:
+      "Aberrant mitochondrial fission in neurons induced by protein kinase C{delta} under oxidative stress conditions in vivo.",
+    journal: "<b>Molecular biology of the cell</b>, 22(2), 256-265.",
+    link: "https://doi.org/10.1091/mbc.E10-06-0551",
+    year: 2011,
   },
 ];
+
+// 专利
 const _patents = [
   {
-    authors: "AAA",
-    title: "BBB",
+    authors:
+      "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., <b>Shen, N.</b>, Robertson, A.S. and Chang, A.N.",
+    title:
+      "Fulcrum Therapeutics Inc, 2023. P38 kinase inhibitors reduce dux4 and downstream gene expression for the treatment of fshd. Japanese Patent Application 2023113691A. ",
+  },
+  {
+    authors:
+      "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., <b>Shen, N.</b>, Robertson, A.S. and Chang, A.N.",
+    title:
+      "Fulcrum Therapeutics Inc, 2020. P38 kinase inhibitors reduce DUX4 and downstream gene expression for the treatment of FSHD. U.S. Patent 10,537,560.",
+  },
+  {
+    authors:
+      "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., <b>Shen, N.</b>, Robertson, A.S. and Chang, A.N.",
+    title:
+      "Fulcrum Therapeutics Inc, 2020. P38 kinase inhibitors reduce DUX4 and downstream gene expression for the treatment of FSHD. Israel Patent Offiece IL273589.",
+  },
+  {
+    authors:
+      "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., <b>Shen, N.</b>, Robertson, A.S. and Chang, A.N.",
+    title:
+      "Fulcrum Therapeutics Inc, 2020. Use of p38 inhibitors to reduce expression of dux4. U.S. Patent 11479770B2.",
+  },
+  {
+    authors:
+      "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., <b>Shen, N.</b>, Robertson, A.S. and Chang, A.N.",
+    title:
+      " Fulcrum Therapeutics Inc, 2019. P38 kinase inhibitors reduce DUX4 and downstream gene expression for the treatment of FSHD. U.S. Patent 10342786.",
+  },
+  {
+    authors:
+      "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., <b>Shen, N.</b>, Robertson, A.S. and Chang, A.N.",
+    title:
+      "Fulcrum Therapeutics Inc, 2019. P38 kinase inhibitors reduce dux4 and downstream gene expression for the treatment of fshd. Japanese Patent Application 7012152.",
+  },
+  {
+    authors:
+      "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., <b>Shen, N.</b>, Robertson, A.S. and Chang, A.N.",
+    title:
+      "Fulcrum Therapeutics Inc, 2019. P38 kinase inhibitors reduce dux4 and downstream gene expression for the treatment of fshd. European Patent Office 3691620T3.",
+  },
+  {
+    authors:
+      "Cacace, A.M., SOTO, L.G.A.R., Thompson III, L.A., Wallace, O.B., Ronco, L.V., <b>Shen, N.</b>, Robertson, A.S. and Chang, A.N.",
+    title:
+      "Fulcrum Therapeutics Inc, 2018. P38 kinase inhibitors reduce dux4 and downstream gene expression for the treatment of fshd. Canadian Intellectual Property Office CA3077499.",
   },
 ];
 const Publish = () => (
   <Layout>
-    <h2>Publications</h2>
-    {_articles.map((item, idx) => (
-      <div className={styles.row} key={idx}>
-        <li>
-          <span dangerouslySetInnerHTML={{ __html: item.authors }}></span>{" "}
-          <span dangerouslySetInnerHTML={{ __html: item.title }}></span>{" "}
-          <span dangerouslySetInnerHTML={{ __html: item.journal }}></span>{" "}
-          <Link href={item.link}>
-            <a>Link</a>
-          </Link>
-        </li>
-      </div>
-    ))}
-    <div className={styles.row}>
-      <li>
-        Liu, Y. #, Zhang, T. #, You, N., Wu, S. *, & <b>Shen, N.*</b> (2024).
-        MAGPIE: accurate pathogenic prediction for multiple variant types using
-        machine learning approach.
-        <b>Genome medicine</b>,16(1),3.{" "}
-        <Link href="https://doi.org/10.1186/s13073-023-01274-4">
-          <a>Link</a>
-        </Link>
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Zhang, T., Jia, H., Song, T., Lv, L., Gulhan, D. C., Wang, H., Guo, W.,
-        Xi, R., Guo, H., & Shen, N. (2023). De novo identification of expressed
-        cancer somatic mutations from single-cell RNA sequencing data.
-        <b>Genome Medicine</b>, 15(1), 115.{" "}
-        <Link href="https://doi.org/10.1186/s13073-023-01269-1">
-          <a>Link</a>
-        </Link>
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Wu, L., Wang, H. S., Chen, Y. N., Zhang, X., Zhang, T.Y., Shen, N., Tao,
-        G. Y., Sun, Z. Q., Ding, Y., Wang, W. L., Bu, J. J., (2023). Beyond
-        radiologist-level liver lesion detection on multi-phase
-        contrast-enhanced CT images by deep learning.
-        <b>iScience</b>, 26 (11): 108183
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Lv, L., Zhang, T.Y., Jia, H.Y., Zhang, Y.Y., Ahsan, A., Zhao, X.Y.,
-        Chen, T., Shen, Z.Q., Shen, N.(2022). Temporally integrated
-        transcriptome analysis reveals ASFV pathology and host response
-        dynamics. <b>Frontiers in Immunology</b>. 13:995998
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Jin, Z., Huang, W., Shen, N., Li, J., Wang, X., Dong, J., Park, P.J.,
-        Xi, R. (2022). Single-cell gene fusion detection by scFusion. Nat
-        Commun, 13(1084).
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Li, J., Cai, Z., Vaites, L. P., Shen, N., Mitchell, D. C., Huttlin, E.
-        L., ... & Gygi, S. P. (2021). Proteome-wide mapping of short-lived
-        proteins in human cells. Molecular Cell, 81(22), 4722-4735.
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Rojas, L.A., Valentine, E., Accorsi, A., Maglio, J., Shen, N.,
-        Robertson, A., Kazmirski, S., Rahl, P., Tawil, R., Cadavid, D.,
-        Thompson, L.A., Ronco, L., Chang, A., Cacace, A., and Wallace, O., 2020.
-        P38α Regulates Expression of DUX4 in Facioscapulohumeral Muscular
-        Dystrophy. Journal of Pharmacology and Experimental Therapeutics.
-        119.264689
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Shen, N., Zhao, J., Schipper, J.L., Zhang, Y., Bepler, T., Leehr, D.,
-        Bradley, J., Horton, J., Lapp, H. and Gordan, R., 2018. Divergence in
-        DNA specificity among paralogous transcription factors contributes to
-        their differential in vivo binding. Cell systems, 6(4), pp.470-483
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Sparks, E.E., Drapek, C.E., Gaudinier, A., Li, S., Ansariola, M., Shen,
-        N., Matsumoto, N., Turco, G., Qiu, Y., Petricka, J.J., Foret, J., Chen,
-        M., Gordân, R., Megraw, M. and Brady, S., Benfey, P.N., 2016.
-        Establishment of expression in the SHORTROOT-SCARECROW transcriptional
-        cascade through opposing activities of both activators and repressors.
-        Developmental Cell 39(5):585-596
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Zhou, T.*, Shen, N.*, Yang, L., Horton, J., Bussemaker, H., Gordân, R.+
-        and Rohs, R+., 2015. Quantitative modeling of transcription factor
-        binding specificities using DNA shape. Proceedings of the National
-        Academy of Science 112(15); 4654-4659
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Gordân, R., Shen, N., Dror, I., Zhou, T., Horton, J., Rohs, R. and
-        Bulky, M.L., 2013. Genomic regions flanking E-box binding sites
-        influence DNA binding specificity of bHLH transcription factors through
-        DNA shape. Cell Reports 3(4):1093-104
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Chen, J., Ma, M., Shen, N., Xi, J. and Tian, W., 2013. Integration of
-        cancer gene co-expression network and metabolic network to uncover
-        potential cancer drug targets. Journal of Proteome Research 12(6):
-        2354-2364
-      </li>
-    </div>
-    <div className={styles.row}>
-      <li>
-        Qi, X., Disatnik, MH., Shen, N., Sobel, RA. and Mochly-Rosen, D., 2011.
-        Aberrant Mitochondrial Fission in Neurons Induced by Delta Protein
-        Kinase C under Oxidative Stress Conditions, in Vivo. Molecular Biology
-        of the Cell 22(2):256-265
-      </li>
-    </div>
-    <h2>Patents</h2>
-    {patents.map((item, idx) => (
-      <div className={styles.row} key={idx}>
-        <li>{item}</li>
-      </div>
-    ))}
-    <h2>Conference Proceedings</h2>
-    {conference_Proceedings.map((item, idx) => (
-      <div className={styles.row} key={idx}>
-        <li>{item}</li>
-      </div>
-    ))}
+    <Grid container>
+      <Grid xs={10}>
+        <h2 id="Articles">Publications</h2>
+        {_articles.map((item, idx) => (
+          <div className={styles.row} key={idx}>
+            <li>
+              <span dangerouslySetInnerHTML={{ __html: item.authors }}></span>{" "}
+              <span dangerouslySetInnerHTML={{ __html: item.title }}></span>{" "}
+              <span dangerouslySetInnerHTML={{ __html: item.journal }}></span>{" "}
+              {/* <Link href={item.link}> */}
+              {/* <a>Link</a> */}
+              {/* <SearchOutlinedIcon></SearchOutlinedIcon> */}
+              {/* </Link> */}
+              <IconButton aria-label="detail" href={item.link} size="small">
+                <SearchIcon />
+              </IconButton>
+            </li>
+          </div>
+        ))}
+        <h2 id="Patents">Patents</h2>
+        {patents.map((item, idx) => (
+          <div className={styles.row} key={idx}>
+            <li>{item}</li>
+          </div>
+        ))}
+        <h2 id="Conference_Proceedings">Conference Proceedings</h2>
+        {conference_Proceedings.map((item, idx) => (
+          <div className={styles.row} key={idx}>
+            <li>{item}</li>
+          </div>
+        ))}
+      </Grid>
+      <Grid xs={2}>
+        <h3>Contents</h3>
+        <nav className={styles.jss123}>
+          <li>
+            <a href="#Articles">Articles ({_articles.length})</a>
+          </li>
+          <li>
+            <a href="#Patents">Patents ({_patents.length})</a>
+          </li>
+          <li>
+            <a href="#Conference_Proceedings">
+              Conference Proceedings ({conference_Proceedings.length})
+            </a>
+          </li>
+        </nav>
+      </Grid>
+    </Grid>
   </Layout>
 );
 export default Publish;
